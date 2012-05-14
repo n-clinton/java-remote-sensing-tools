b6416d0e7592da5d6605bdd17db6bca685ab1baf
nicholas.clinton@gmail.com <nicholas.clinton@gmail.com@959bb53c-d814-e0a8-b6a9-031716d00e41>
2012-05-14 11:35:43 +0000

    From Mac

--- trunk/Open/src/com/berkenviro/imageprocessing/WekaUtils.java
+++ trunk/Open/src/com/berkenviro/imageprocessing/WekaUtils.java
@@ -24,18 +24,28 @@
  */
 package com.berkenviro.imageprocessing;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StreamTokenizer;
 import java.util.Enumeration;
+import java.util.Random;
 
+import weka.attributeSelection.ASEvaluation;
+import weka.attributeSelection.ASSearch;
+import weka.attributeSelection.AttributeSelection;
+import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.CSVLoader;
+import weka.core.converters.ConverterUtils;
 import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Discretize;
 import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.instance.Resample;
 
 /**
  * Various utilities using the Weka API.
@@ -130,29 +140,78 @@ public class WekaUtils {
 	 * @param in
 	 * @param toRemove is the attribute indices to remove
 	 */
-	public static Instances removeAttributes(Instances in, int[] toRemove) {
-		Instances out = null;
-		
+	public static Instances removeAttributes(Instances in, int[] toRemove) throws Exception {
 		Remove rm = new Remove();
 		rm.setAttributeIndicesArray(toRemove);
-
-		try {
-			rm.setInputFormat(in);
-			rm.setInvertSelection(true);
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
-
-		try {
-			out = Filter.useFilter(in, rm);
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
+		rm.setInputFormat(in);
+		rm.setInvertSelection(true);
+		return Filter.useFilter(in, rm);
+	}
+	
+	/**
+	 * 
+	 * @param percent
+	 * @param in
+	 * @return
+	 * @throws Exception
+	 */
+	public static Instances[] subset(int percent, Instances in) throws Exception {
+		Instances[] out = new Instances[2];
+		in.randomize(new Random());
+		int subsetSize = (int)Math.round(in.numInstances()*(percent/100.0));
+		out[0] = new Instances(in, 0, subsetSize);
+		out[1] = new Instances(in, subsetSize, in.numInstances()-subsetSize);
 		return out;
 	}
 	
+	/**
+	 * 
+	 * @param in
+	 * @param classatt
+	 * @return
+	 */
+	public static Instances filterMissing(Instances in, String classatt) {
+		Instances out = new Instances(in);
+		out.setClass(out.attribute(classatt));
+		out.deleteWithMissingClass();
+		return out;
+	}
 	
+	/**
+	 * 
+	 * @param in
+	 * @param classatt
+	 * @return
+	 * @throws Exception
+	 */
+	public static Instances discretize(Instances in, String classatt) throws Exception {
+		Discretize dis = new Discretize();
+		dis.setAttributeIndicesArray(new int[] {in.attribute(classatt).index()});
+		dis.setInputFormat(in);
+		dis.setBins(20);
+		dis.setUseEqualFrequency(true);
+		return Filter.useFilter(in, dis);
+	}
 	
+	/**
+	 * 
+	 * @param in
+	 * @param eval
+	 * @param search
+	 * @return
+	 * @throws Exception
+	 */
+	public static double[][] getRanking(Instances in, ASEvaluation eval, ASSearch search) throws Exception {
+	  AttributeSelection attsel = new AttributeSelection();
+	  // defaults
+	  //CfsSubsetEval eval = new CfsSubsetEval();
+	  //GreedyStepwise search = new GreedyStepwise();
+	  //search.setSearchBackwards(true);
+	  attsel.setEvaluator(eval);
+	  attsel.setSearch(search);
+	  attsel.SelectAttributes(in);
+	  return attsel.rankedAttributes();
+	}
 	
 	/**
 	 * Test code and processing log.
@@ -183,29 +242,135 @@ public class WekaUtils {
 		writeArff(combo, "F:\\Cheatgrass2008\\training\\Makr_0506_combo.arff");
 		*/
 		
-		Instances inst1 = loadArff("F:\\Cheatgrass2008\\training\\p37r32_2005_id.arff");
-		Instances inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r34_2005_id.arff");
-		Instances combo = combineInstances(inst1, inst2);
+//		Instances inst1 = loadArff("F:\\Cheatgrass2008\\training\\p37r32_2005_id.arff");
+//		Instances inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r34_2005_id.arff");
+//		Instances combo = combineInstances(inst1, inst2);
+//		
+//		inst1 = combo;
+//		inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r31_2006_id.arff");
+//		combo = combineInstances(inst1, inst2);
+//		
+//		inst1 = combo;
+//		inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r32_2006_id.arff");
+//		combo = combineInstances(inst1, inst2);
+//		
+//		inst1 = combo;
+//		inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r33_2006_id.arff");
+//		combo = combineInstances(inst1, inst2);
+//		
+//		inst1 = combo;
+//		inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r34_2006_id.arff");
+//		combo = combineInstances(inst1, inst2);
+//		
+//		writeArff(combo, "F:\\Cheatgrass2008\\training\\Makr_0506_combo_id.arff");
 		
-		inst1 = combo;
-		inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r31_2006_id.arff");
-		combo = combineInstances(inst1, inst2);
-		
-		inst1 = combo;
-		inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r32_2006_id.arff");
-		combo = combineInstances(inst1, inst2);
-		
-		inst1 = combo;
-		inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r33_2006_id.arff");
-		combo = combineInstances(inst1, inst2);
-		
-		inst1 = combo;
-		inst2 = loadArff("F:\\Cheatgrass2008\\training\\p38r34_2006_id.arff");
-		combo = combineInstances(inst1, inst2);
-		
-		writeArff(combo, "F:\\Cheatgrass2008\\training\\Makr_0506_combo_id.arff");
+		// 20120426 split the urban instances
+//		String filename = "/Users/nclinton/Documents/gpt2id_lattices_joined2_pop_nightlights_no_zeros.arff";
+//		Instances input = loadArff(filename);
+//		Instances[] split = null;
+//		try {
+//			split = subset(70, input);
+//			System.out.println(split[0].toSummaryString());
+//			System.out.println(split[1].toSummaryString());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		writeArff(split[0], "/Users/nclinton/Documents/gpt2id_lattices_joined2_pop_nightlights_no_zeros_70.arff");
+//		writeArff(split[1], "/Users/nclinton/Documents/gpt2id_lattices_joined2_pop_nightlights_no_zeros_30.arff");
 		
 		
+		// GLA14
+//		String filename = "/Users/nclinton/Documents/urban/lidar/GLA14_r33_mssu_points_gpt2id_joined_nightlights_no_zeros.arff";
+//		Instances input = loadArff(filename);
+//		Instances[] split = null;
+//		try {
+//			split = subset(70, input);
+//			System.out.println(split[0].toSummaryString());
+//			System.out.println(split[1].toSummaryString());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		writeArff(split[0], "/Users/nclinton/Documents/urban/lidar/GLA14_r33_mssu_points_gpt2id_joined_nightlights_no_zeros_70.arff");
+//		writeArff(split[1], "/Users/nclinton/Documents/urban/lidar/GLA14_r33_mssu_points_gpt2id_joined_nightlights_no_zeros_30.arff");
+		
+//		String filename = "/Users/nclinton/Documents/urban/gpt2id_lattices_joined3_cdd_attributes_pop_nightlights_no_zeros.arff";
+//		Instances input = loadArff(filename);
+//		Instances[] split = null;
+//		try {
+//			split = subset(70, input);
+//			System.out.println(split[0].toSummaryString());
+//			System.out.println(split[1].toSummaryString());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		writeArff(split[0], "/Users/nclinton/Documents/urban/gpt2id_lattices_joined3_cdd_attributes_pop_nightlights_no_zeros_70.arff");
+//		writeArff(split[1], "/Users/nclinton/Documents/urban/gpt2id_lattices_joined3_cdd_attributes_pop_nightlights_no_zeros_30.arff");
+		
+		
+//		String mssu = "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_h2o_no_zeros.csv";
+//		Instances inst = loadCSV(mssu);
+//		System.out.println(inst.toSummaryString());
+//		Instances filtered = filterMissing(inst, "uhi");
+//		System.out.println(filtered.toSummaryString());
+		
+//		String filename = "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_h2o_no_zeros.csv";
+//		Instances input = loadCSV(filename);
+//		Instances[] split = null;
+//		try {
+//			split = subset(70, input);
+//			System.out.println(split[0].toSummaryString());
+//			System.out.println(split[1].toSummaryString());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		writeArff(split[0], "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_h2o_no_zeros_70.arff");
+//		writeArff(split[1], "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_h2o_no_zeros_30.arff");
+		
+//		String filename = "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_h2o_no_zeros_70.arff";
+//		Instances in = loadArff(filename);
+//		// uhi
+//		try {
+//			//Instances filt = removeAttributes(in, new int[] {0,1,10,12,13,14,15,16}); // uhi
+//			//Instances filt = removeAttributes(in, new int[] {0,1,9,10,11,12,13,15}); // uhs
+//			//Instances rem = filterMissing(filt, "uhs_1");
+//			//writeArff(rem, "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_h2o_no_zeros_70_uhs.arff");
+//			Instances rem = loadArff("/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_h2o_no_zeros_70_uhi.arff");
+//			System.out.println("Discretizing...");
+//			Instances disc = discretize(rem, "uhi");
+//			System.out.println(disc.toSummaryString());
+//			writeArff(disc, "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_h2o_no_zeros_70_uhi_discrete.arff");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		
+		String filename = "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_cdd_h2o_no_zeros.csv";
+		Instances input = loadCSV(filename);
+		
+		try {
+//			Instances[] split = subset(70, input);
+//			System.out.println(split[0].toSummaryString());
+//			System.out.println(split[1].toSummaryString());
+//			writeArff(split[0], "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_cdd_h2o_no_zeros_70.arff");
+//			writeArff(split[1], "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_cdd_h2o_no_zeros_30.arff");
+//			
+//			Instances filt = removeAttributes(split[0], new int[] {0,1,10,12,13,14,15,16}); // uhi
+//			Instances rem = filterMissing(filt, "uhi");
+//			writeArff(rem, "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_cdd_h2o_no_zeros_70_uhi.arff");
+//			System.out.println("Discretizing...");
+//			Instances disc = discretize(rem, "uhi");
+//			writeArff(disc, "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_cdd_h2o_no_zeros_70_uhi_discrete.arff");
+//			
+//			filt = removeAttributes(split[0], new int[] {0,1,9,10,11,12,13,15}); // uhs
+//			rem = filterMissing(filt, "uhs_1");
+//			writeArff(rem, "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_cdd_h2o_no_zeros_70_uhs.arff");
+//			System.out.println("Discretizing...");
+//			disc = discretize(rem, "uhs_1");
+//			writeArff(disc, "/Users/nclinton/Documents/urban/gpt2id_lattices_joined4_cdd_h2o_no_zeros_70_uhs_discrete.arff");
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
